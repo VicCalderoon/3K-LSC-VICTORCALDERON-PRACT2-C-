069477e3b4e7928be0e2ad7aab7222b0cd37c090
VicCalderoon <146136453+VicCalderoon@users.noreply.github.com>
2023-10-04 14:16:16 -0700

    Agregar archivos de proyecto.

---
+++ 3K-LSC-VICTORCALDERON-PRACT2-C#/Program.cs
@@ -0,0 +1,26 @@
+﻿// VICTOR CALDERON- SUM DE 8 DIGITOS EN C#
+
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+class Program
+{
+    const int NUM = 8;
+    static void Main()
+    {
+        int[] nums = new int[NUM];
+        int total = 0;
+        for (int i = 0; i < NUM; i++)
+        {
+            Console.Write("por favor introduzca, el numero:");
+            nums[i] = int.Parse(Console.ReadLine());
+            total += nums[i];
+        }
+        Console.WriteLine("El total de numeros es:" + total);
+        Console.ReadKey();
+    }
+}
